9abfedd3d01f4c73931f4685b8fedf6863b829a6
rishi-srivastav1706 <rishiaiweb@gmail.com>
2025-09-07 14:32:34 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,37 @@
+import calc_art
+
+
+operations = {
+    "+": "add",
+    "-": "substract",
+    "*": "multiply",
+    "/": "divide"
+}
+
+# print(operations["*"](4, 8))
+
+
+def calculator():
+    print(calc_art.logo)
+    should_accumulate = True
+    num1 = float(input("What is the first number?: "))
+
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number?: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
+
+
+calculator()
