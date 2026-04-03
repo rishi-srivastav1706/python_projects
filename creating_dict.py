d3f6c3be72d53788baa4c73f3d6b08c974685eae
Rishi Srivastav <rishiaiweb@gmail.com>
2026-04-03 22:48:13 +0530

    Add files via upload

---
+++ creating_dict.py
@@ -0,0 +1,9 @@
+words =["apple","banana","kiwi","cherry","mango"]
+words_dict = {
+    "apple": len(words[0]),
+    "banana": len(words[1]),
+    "kiwi": len(words[2]),
+    "cherry": len(words[3]),
+    "mango": len(words[4])
+}
+print(words_dict)
